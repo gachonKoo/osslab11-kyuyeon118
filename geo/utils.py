abee647aae09cd738636d2039f5fafb217fc7d99
kyuyeon <joymaker1523@gmail.com>
2025-11-16 21:29:52 +0900

    Lab 11 done

---
+++ geo/utils.py
@@ -0,0 +1,9 @@
+import math
+
+def area_circle(r):
+    """반지름 r인 원의 넓이를 계산"""
+    return math.pi * (r ** 2)
+
+def perimeter_circle(r):
+    """반지름 r인 원의 둘레를 계산"""
+    return 2 * math.pi * r
---
+++ tester.py
@@ -0,0 +1,7 @@
+# tester.py
+from geo import utils
+
+if __name__ == "__main__":
+    r = 5
+    print("Area:", utils.area_circle(r))
+    print("Perimeter:", utils.perimeter_circle(r))
